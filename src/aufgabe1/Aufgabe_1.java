b33cf70c7d1e0d43c55d3f7913db45bf8128f530
Superguyplayer <perhenrik.phs@gmail.com>
2021-01-25 12:33:42 +0100

    Aufgabe_1 fertig

--- src/aufgabe1/Aufgabe_1.java
+++ src/aufgabe1/Aufgabe_1.java
@@ -9,6 +9,8 @@ public class Aufgabe_1 {
         int age;
         double height;
         Scanner scan = new Scanner(System.in);
+
+
         System.out.println("Bitte geben Sie Ihren Namen ein.");
         name = scan.nextLine();
         System.out.println("Bitte geben Sie Ihr Alter ein.");
@@ -16,7 +18,8 @@ public class Aufgabe_1 {
         System.out.println("Bitte geben Sie Ihre Größe in Metern ein.");
         height = scan.nextDouble();
 
-        System.out.println("Hallo " + name + "!\tSie sind " + age + " Jahre alt und " + height + "m groß.");
+        //Output
+        System.out.println("Hallo " + name + "!\nSie sind " + age + " Jahre alt und " + height + "m groß.");
     }
 
 }
