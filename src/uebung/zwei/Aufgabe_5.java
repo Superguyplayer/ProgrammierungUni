1a99f118a230c1d57112b69f80d5960b721bb610
Superguyplayer <perhenrik.phs@gmail.com>
2021-01-29 11:20:15 +0100

    scan closed

--- src/uebung/zwei/Aufgabe_5.java
+++ src/uebung/zwei/Aufgabe_5.java
@@ -15,10 +15,12 @@ public class Aufgabe_5 {
         System.out.print("Laufzeit in Jahren: ");
         laufzeit = scan.nextInt();
 
+        scan.close();
 
         endKapital = kapital + (zinsSatz * kapital / 100) * laufzeit;
 
         System.out.println("Ihr Endkapital beträgt: " + endKapital + "€");
+
 
     }
 }
