fa37c16ec44ae48c96574de00971c85e6b661ddf
Superguyplayer <perhenrik.phs@gmail.com>
2021-02-01 11:05:58 +0100

    Aufgabe_4b

---
+++ src/vorlesung/v3/Aufgabe_4b.java
@@ -0,0 +1,19 @@
+package vorlesung.v3;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Aufgabe_4b {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bitte geben sie Ihr Alter ein: ");
+        int age = scanner.nextInt();
+
+        switch (age) {
+            case 18 -> System.out.println("Sie sind volljährig");
+            case 16 -> System.out.println("Du darfst Mofa fahren");
+            default -> System.out.println("Das ist kein besonderes Alter");
+        }
+    }
+}
