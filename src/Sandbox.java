6f95449974f04caab8885e4b3e454aa55d36f9f6
Superguyplayer <perhenrik.phs@gmail.com>
2021-02-24 09:31:30 +0100

    Sandbox

---
+++ src/Sandbox.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Sandbox {
+    public static void main(String[] args) {
+        int[] hi = {1,2,5,6,3};
+        System.out.println(Arrays.toString(hi));
+    }
+}
