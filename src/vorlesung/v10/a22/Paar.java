324fe2e3c00b4654bdde76cd26eb1d2ca7b4e689
Superguyplayer <perhenrik.phs@gmail.com>
2021-03-29 12:29:50 +0200

    a22 + a23

---
+++ src/vorlesung/v10/a22/Paar.java
@@ -0,0 +1,30 @@
+package vorlesung.v10.a22;
+
+public class Paar<T, U> {
+
+    protected T valueT;
+    protected U valueU;
+
+    public void setValueT(T valueT) {
+        this.valueT = valueT;
+    }
+
+    public T getValueT() {
+        return valueT;
+    }
+
+    public void setValueU(U valueU) {
+        this.valueU = valueU;
+    }
+
+    public U getValueU() {
+        return valueU;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + valueT +
+                ", " + valueU +
+                ')';
+    }
+}
