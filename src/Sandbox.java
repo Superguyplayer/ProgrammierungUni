0e7e8b899288a33518752739fe9daa6ab5f30159
Superguyplayer <perhenrik.phs@gmail.com>
2021-09-13 10:41:18 +0200

    neue aufgabe db

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -1,8 +1,15 @@
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Sandbox {
-    public static void main(String[] args)  {
-        System.out.println(17+4*2);
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://superguyplayer.ddns.net:3306/world?user=root&password=Antonia1,");
+
+
+
+
     }
 
 
